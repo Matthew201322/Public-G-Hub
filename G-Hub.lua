17069af39937e27a3af08e1860c61614c114467b
Grassful <92701676+Matthew201322@users.noreply.github.com>
2024-12-10 20:33:16 -0600

    Update G-Hub.lua

--- G-Hub.lua
+++ G-Hub.lua
@@ -1,45 +1,24 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local Window = Rayfield:CreateWindow({
-   Name = "G-Hub Installation",
-   Icon = 8676495068, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Hub Install",
-   LoadingSubtitle = "jfgjdnnf",
-   Theme = "Ocean", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Installation", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
 
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = true, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = true, -- Create a custom folder for your hub/game
-      FileName = "G-Hub"
-   },
-
-   Discord = {
-      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "https://discord.gg/fyMfKh8d49", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = false -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Key System!",
-      Subtitle = "released",
-      Note = "jfjfjdncg", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"jfkgrjjdkgjrjdjg"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+local Tab = Window:MakeTab({
+	Name = "Install",
+	Icon = "",
+	PremiumOnly = false
 })
 
-local Tab = Window:CreateTab("Installation", "rewind")
-
-local Button = Tab:CreateButton({
-   Name = "Install",
-   Callback = function()
-   Rayfield:Destroy()
-   wait(0.5)
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Matthew201322/G-Hub/refs/heads/main/G-Hub.lua?token=GHSAT0AAAAAAC3THLORPZCKQ47LLULO47QIZ2YZTPQ", true))()
-   end,
+Tab:AddButton({
+	Name = "GET IT NEOWWW!1!1!1",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/Matthew201322/G-Hub/refs/heads/main/G-Hub.lua?token=GHSAT0AAAAAAC3THLORPZCKQ47LLULO47QIZ2YZTPQ", true))()
+  	end    
 })
+
+Tab:AddButton({
+	Name = "Unload",
+	Callback = function()
+      		OrionLib:Destroy()
+  	end    
+})
+
+OrionLib:Init()
